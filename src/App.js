265734a76d6729130e04fc41dfaf3b0985a3c450
Adarsh Ram <adarshram333@gmail.com>
2020-11-15 13:26:15 +0530

    Post Component UI done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <div className="app__header">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRyhmvK81dnLxY5eVy1QpqtGTgpCtj7Gva6nw&usqp=CAU" alt="" className="app__headerImage"/>
       </div>
+      <Post userName="user123" caption="Wow props"imageUrl="https://images.unsplash.com/photo-1605290975464-72d2acef7d4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></Post>
       <Post></Post>
       {/* //Posts */}
 
--- src/components/PostComponent/Post.js
+++ src/components/PostComponent/Post.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import './Post.css'
+import Avatar from "@material-ui/core/Avatar"
 
 const Post = () => {
     return (
-        <div>
+        <div className="post">
             {/* header -> pic + username */}
-            <h3>Username</h3>
+            <div className="post__header">
+                <Avatar className="post__avatar" alt="userName" src="https://images.unsplash.com/photo-1605020798729-c2486b2971b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
+
+                </Avatar>
+                <h3>User124</h3>
+            </div>
+          
 
             {/* image */}
-
+                <img className="post__image" src="https://images.unsplash.com/photo-1605286998753-040e16ab0c82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" alt=""/>
 
             {/* username and caption */}
-
+                <h4 className="post__text"><strong>User name</strong>caption</h4>
         </div>
     )
 }
