2dc425cadbaa04339e9e239a0f0a1a05f8771f14
Adarsh Ram <adarshram333@gmail.com>
2020-11-15 20:34:03 +0530

    setup created to dynamically display posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import React, { useState } from 'react';
 import Post from './components/PostComponent/Post'
 function App() {
+
+  //state
+
+  const [posts,setPosts] = useState([
+    {userName:"user123",
+     caption:"Wow props",
+     imageUrl:"https://images.unsplash.com/photo-1605290975464-72d2acef7d4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    }
+  ])
   return (
     <div className="app">
 
@@ -8,9 +18,17 @@ function App() {
       <div className="app__header">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRyhmvK81dnLxY5eVy1QpqtGTgpCtj7Gva6nw&usqp=CAU" alt="" className="app__headerImage"/>
       </div>
-      <Post userName="user123" caption="Wow props"imageUrl="https://images.unsplash.com/photo-1605290975464-72d2acef7d4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"></Post>
-      <Post></Post>
-      {/* //Posts */}
+
+      {/* Iterating through posts state and rendering posts dynamically */}
+      {
+        posts.map( post =>
+          <Post userName={post.userName}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+          ></Post>
+        )
+      }
+      
 
 
     </div>
--- src/components/PostComponent/Post.js
+++ src/components/PostComponent/Post.js
@@ -2,23 +2,23 @@ import React from 'react'
 import './Post.css'
 import Avatar from "@material-ui/core/Avatar"
 
-const Post = () => {
+const Post = ({userName,caption,imageUrl}) => {
     return (
         <div className="post">
             {/* header -> pic + username */}
             <div className="post__header">
-                <Avatar className="post__avatar" alt="userName" src="https://images.unsplash.com/photo-1605020798729-c2486b2971b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60">
+                <Avatar className="post__avatar" alt={userName} src="">
 
                 </Avatar>
-                <h3>User124</h3>
+                <h3>{userName}</h3>
             </div>
           
 
             {/* image */}
-                <img className="post__image" src="https://images.unsplash.com/photo-1605286998753-040e16ab0c82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" alt=""/>
+                <img className="post__image" src={imageUrl} alt=""/>
 
             {/* username and caption */}
-                <h4 className="post__text"><strong>User name</strong>caption</h4>
+    <h4 className="post__text"><strong>{userName}</strong>{caption}</h4>
         </div>
     )
 }
