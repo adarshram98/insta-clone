8867742f6ff1604930cc672f466ef24d1650ceed
Adarsh Ram <adarshram333@gmail.com>
2020-11-15 10:30:23 +0530

    post component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import Post from './components/PostComponent/Post'
 function App() {
   return (
-    <div className="App">
-      hey
+    <div className="app">
+
+      {/* header */}
+      <div className="app__header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRyhmvK81dnLxY5eVy1QpqtGTgpCtj7Gva6nw&usqp=CAU" alt="" className="app__headerImage"/>
+      </div>
+      <Post></Post>
+      {/* //Posts */}
+
+
     </div>
   );
 }
---
+++ src/components/PostComponent/Post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div>
+            {/* header -> pic + username */}
+            <h3>Username</h3>
+
+            {/* image */}
+
+
+            {/* username and caption */}
+
+        </div>
+    )
+}
+
+export default Post
